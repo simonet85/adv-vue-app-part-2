2a4801da8223e34c3943ab9fb2939cac9ac918c0
simonet85 <christian.85@live.fr>
2020-08-10 00:01:32 +0200

    Create Child component TodoItem and moved the functionalities from TodoList

---
+++ src/components/TodoItem.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="todo-item">
+    <div class="todo-item-left">
+      <input type="checkbox" v-model="todo.completed" />
+      <div
+        v-if="!todo.editing"
+        class="todo-item-label"
+        :class="{ completed: todo.completed }"
+        @dblclick="editTodo(todo)"
+      >
+        {{ todo.title }}
+      </div>
+      <input
+        v-else
+        type="text"
+        class="todo-item-edit"
+        v-model="todo.title"
+        @blur="doneTodo(todo)"
+        @keyup.enter="doneTodo(todo)"
+        @keyup.esc="cancelEdit(todo)"
+        v-focus
+      />
+    </div>
+
+    <div class="remove-item" @click="deleteTodo(index)">
+      &times;
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "todo-item",
+  props:{
+      todo:{
+          type:Object,
+          required: true
+      },
+      index:{
+          type: Number,
+          required: true
+      }
+  }
+};
+</script>
+
+<style scoped></style>
